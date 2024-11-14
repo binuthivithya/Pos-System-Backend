8c59d57e69030d3fbb47724361254f03925eaea0
Binu Thivithya <binuedirisinghe@gmail.com>
2024-11-14 17:43:39 +0530

    git init

---
+++ src/test/java/com/ijse/cmjdfinal/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijse.cmjdfinal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
